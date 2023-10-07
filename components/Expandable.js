e67ca0528ff1e3070e8f98e2a575da7db6d178f5
Nick Lowman <nlowman20@gmail.com>
2023-10-07 18:10:23 +1100

    Formatting

--- components/Expandable.js
+++ components/Expandable.js
@@ -1,20 +1,26 @@
-import { render } from 'storyblok-rich-text-react-renderer';
-import { Icon } from '@iconify/react';
-import { useState } from 'react';
+import { render } from "storyblok-rich-text-react-renderer";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
 
 const Expandable = ({ data }) => {
-
-    const [isExpanded, setIsExpanded] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <div className="my-6 p-8 shadow-lg shadow-gray-950 rounded flex flex-col hover:cursor-pointer" onClick={() => setIsExpanded(!isExpanded)}>
-        <h3 className="text-2xl text-black font-bold mb-4">{data.heading}</h3>
-        {isExpanded && (
-            render(data.content)
-        )}
-        <div className="flex justify-between">
-            <p className="text-melon font-semibold uppercase text-sm">{isExpanded ? 'Read Less' : 'Read More'}</p>
-            <Icon icon={isExpanded ? "dashicons:arrow-up-alt2" : "dashicons:arrow-down-alt2"} />
-        </div>
+    <div
+      className="my-6 p-8 shadow-lg shadow-gray-950 rounded flex flex-col hover:cursor-pointer"
+      onClick={() => setIsExpanded(!isExpanded)}
+    >
+      <h3 className="text-2xl text-black font-bold mb-4">{data.heading}</h3>
+      {isExpanded && render(data.content)}
+      <div className="flex justify-between">
+        <p className="text-melon font-semibold uppercase text-sm">
+          {isExpanded ? "Read Less" : "Read More"}
+        </p>
+        <Icon
+          icon={
+            isExpanded ? "dashicons:arrow-up-alt2" : "dashicons:arrow-down-alt2"
+          }
+        />
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -12,37 +12,50 @@ import {
 
 export default function Home({ story }) {
   story = useStoryblokState(story);
-  const nav = story.content.main_navigation[0]
-  const welcomeScreen = story.content.welcome_screen[0]
+  const nav = story.content.main_navigation[0];
+  const welcomeScreen = story.content.welcome_screen[0];
 
-  const [showWelcome, setShowWelcome] = useState(false)
-  
+  const [showWelcome, setShowWelcome] = useState(false);
+
   useEffect(() => {
     if (!sessionStorage.getItem("hasSeenWelcome")) {
-      setShowWelcome(true)
-      document.body.style.overflow = 'hidden';
-    } else {
-      setShowWelcome(false)
-      document.body.style.overflow = 'unset';
+      setShowWelcome(true);
+      sessionStorage.setItem("hasSeenWelcome", true);
+      document.body.style.overflow = "hidden";
     }
-  },[])
+  }, []);
 
   useEffect(() => {
-    document.body.style.overflow = showWelcome ? 'hidden' : 'unset'
-    sessionStorage.setItem("hasSeenWelcome", !showWelcome);
-  }, [showWelcome])
+    if (showWelcome) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [showWelcome]);
 
-  if(showWelcome) {
+  if (showWelcome) {
     return (
-    <div className={styles.WelcomeScreen} style={{backgroundImage: `url(${welcomeScreen.images[0].filename})`}}>
-      <img className="max-h-[125px]" height="125px" src={welcomeScreen.logo.filename} />
+      <div
+        className={styles.WelcomeScreen}
+        style={{ backgroundImage: `url(${welcomeScreen.images[0].filename})` }}
+      >
+        <img
+          className="max-h-[125px]"
+          height="125px"
+          src={welcomeScreen.logo.filename}
+        />
         <div className="text-center text-3xl mt-4 lg:mt-8  font-serif">
           {render(welcomeScreen.title)}
         </div>
-        <button className="border border-melon mt-4 lg:mt-8  rounded-full py-4 px-8 text-melon" onClick={() => setShowWelcome(false)}>{welcomeScreen.cta_text}</button>
-    </div>
-  )
-}
+        <button
+          className="border border-melon mt-4 lg:mt-8  rounded-full py-4 px-8 text-melon"
+          onClick={() => setShowWelcome(false)}
+        >
+          {welcomeScreen.cta_text}
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.container}>
@@ -56,15 +69,29 @@ export default function Home({ story }) {
         <h1>{story ? story.name : "My Site"}</h1>
       </header> */}
 
-    {showWelcome && (
-      <div className={styles.WelcomeScreen} style={{backgroundImage: `url(${welcomeScreen.images[0].filename})`}}>
-        <img className="max-h-[125px]" height="125px" src={welcomeScreen.logo.filename} />
+      {showWelcome && (
+        <div
+          className={styles.WelcomeScreen}
+          style={{
+            backgroundImage: `url(${welcomeScreen.images[0].filename})`,
+          }}
+        >
+          <img
+            className="max-h-[125px]"
+            height="125px"
+            src={welcomeScreen.logo.filename}
+          />
           <div className="text-center mt-4 lg:mt-8 text-3xl font-serif">
             {render(welcomeScreen.title)}
           </div>
-          <button className="border border-melon rounded-full mt-4 lg:mt-8 py-4 px-8 text-melon" onClick={() => setShowWelcome(false)}>{welcomeScreen.cta_text}</button>
-      </div>
-    )}
+          <button
+            className="border border-melon rounded-full mt-4 lg:mt-8 py-4 px-8 text-melon"
+            onClick={() => setShowWelcome(false)}
+          >
+            {welcomeScreen.cta_text}
+          </button>
+        </div>
+      )}
 
       <StoryblokComponent blok={story.content} />
     </div>
@@ -76,7 +103,7 @@ export async function getStaticProps() {
 
   let sbParams = {
     version: "published", // or 'draft'
-    cv: new Date().getTime()
+    cv: new Date().getTime(),
   };
 
   const storyblokApi = getStoryblokApi();
