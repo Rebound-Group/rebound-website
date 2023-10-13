1d2ab8e796254cc36ddc19348a160af10d6222f1
Nick Lowman <nlowman20@gmail.com>
2023-10-13 19:17:26 +1100

    Final mobile padding margin and alignment tweaks

--- components/BecomeARebounder.js
+++ components/BecomeARebounder.js
@@ -1,15 +1,24 @@
-import { render } from 'storyblok-rich-text-react-renderer';
+import { render } from "storyblok-rich-text-react-renderer";
 
 const BecomeARebounder = ({ blok }) => {
   return (
-    <div className="p-4 md:p-8 bg-gray-extra-light" >
+    <div className="p-4 md:p-8 bg-gray-extra-light">
       <div className="wrapper my-8 py-8 px-8 md:px-12 bg-black flex flex-col bg-dark-green rounded-2xl text-white">
-        <div className="mb-4" style={{ fontSize: "55px"}}>{render(blok.title)}</div>
-        <div className='flex xs:flex-col md:flex-row md:justify-between items-start md:items-basline'>
-          <div className='mb-4 xs:w-full md:max-w-[60%]'>{render(blok.text)}</div>
-          <a className="bg-melon right text-white text-size max-w-content py-4 px-8 md:ml-8 rounded-full" href={blok.cta_link.url}>{render(blok.cta_text)}</a>
+        <div className="mb-4" style={{ fontSize: "55px" }}>
+          {render(blok.title)}
         </div>
+        <div className="relative flex xs:flex-col md:flex-row md:justify-between items-start md:items-baseline">
+          <div className="mb-4 xs:w-full md:max-w-[60%]">
+            {render(blok.text)}
+          </div>
+          <a
+            className="md:absolute md:bottom-0 md:right-0 bg-melon right text-white text-size max-w-content py-4 px-8 md:ml-8 rounded-full"
+            href={blok.cta_link.url}
+          >
+            {render(blok.cta_text)}
+          </a>
         </div>
+      </div>
     </div>
   );
 };
--- components/our-goal/OurGoalProjectCarouselSlide.js
+++ components/our-goal/OurGoalProjectCarouselSlide.js
@@ -1,12 +1,22 @@
-import { render } from 'storyblok-rich-text-react-renderer';
+import { render } from "storyblok-rich-text-react-renderer";
 
 const OurGoalProjectCarouselSlide = ({ slide }) => {
-
   return (
-    <div className="p-8 shrink-0 flex-1 bg-white text-center md:min-h-[400px] flex flex-col justify-center items-center shadow-xl border border-slate-300" style={{ boxShadow:"0px 45.8042px 62.46026px 0px rgba(49, 49, 49, 0.10), 0px 1.66561px 1.66561px 0px rgba(0, 0, 0, 0.54)" }}>
-        <div className="mb-4"><img style={{height: "80px", width: "80px"}} src={slide.icon.filename} /></div>
-        <div className="mb-4 text-3xl font-bold">{render(slide.title)}</div>
-        <div className=''>{render(slide.text)}</div>
+    <div
+      className="p-4 md:p-8 shrink-0 flex-1 bg-white text-center md:min-h-[400px] flex flex-col justify-center items-center shadow-xl border border-slate-300"
+      style={{
+        boxShadow:
+          "0px 45.8042px 62.46026px 0px rgba(49, 49, 49, 0.10), 0px 1.66561px 1.66561px 0px rgba(0, 0, 0, 0.54)",
+      }}
+    >
+      <div className="mb-4">
+        <img
+          style={{ height: "80px", width: "80px" }}
+          src={slide.icon.filename}
+        />
+      </div>
+      <div className="mb-4 text-3xl font-bold">{render(slide.title)}</div>
+      <div className="">{render(slide.text)}</div>
     </div>
   );
 };
