bbcbadd39abb5701055047cb76db5443cb775db8
Nck Lowman <nlowman20@gmail.com>
2023-10-26 08:32:20 +1100

    Team member layout

--- components/our-team/OurTeamGrid.js
+++ components/our-team/OurTeamGrid.js
@@ -1,9 +1,8 @@
-import { render } from "storyblok-rich-text-react-renderer";
 import TeamMember from "./TeamMember";
 
 const OurTeamGrid = ({ blok }) => {
   return (
-    <section className="grid shadow-lg shadow-gray-950 xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+    <section className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
       {blok.image_grid_items.map((item, i) => (
         <TeamMember blok={item} />
       ))}
--- components/our-team/TeamMember.js
+++ components/our-team/TeamMember.js
@@ -2,16 +2,16 @@ import { render } from "storyblok-rich-text-react-renderer";
 import styles from "./TeamMember.module.css";
 
 const TeamMember = ({ blok }) => {
-  <div
-    className={styles.TeamMember}
-    style={{ backgroundImage: `url(${blok.Image.filename})` }}
-  >
-    <div className="text-4xl mb-0">{render(blok.Name)}</div>
-    <div className="text-4xl mb-0">{render(blok.Title)}</div>
-    <div className="text-4xl mb-4">
-      {blok.Description & render(blok.Description)}
+  return (
+    <div className={styles.TeamMember}>
+      <img src={blok.Image.filename} />
+      <div className="text-4xl mb-0">{render(blok.Name)}</div>
+      <div className="text-4xl mb-0">{render(blok.Title)}</div>
+      <div className="text-4xl mb-4">
+        {blok.Description & render(blok.Description)}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default TeamMember;
