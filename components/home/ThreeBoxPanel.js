54f6433588e568fef24837020c0f85b233f7be20
Nck Lowman <nlowman20@gmail.com>
2023-11-27 07:34:51 +1100

    Desktop layout of three boxes

--- components/home/ThreeBoxPanel.js
+++ components/home/ThreeBoxPanel.js
@@ -6,36 +6,43 @@ const ThreeBoxPanel = ({ blok }) => {
       <div
         className="flex justify-center items-start text-center pt-4"
         style={{
-          minHeight: "282px",
+          minHeight: "500px",
           backgroundImage: `url(${blok.Background.filename})`,
           backgroundSize: "cover",
         }}
       >
-        <div className="flex flex-col">
-          <div className="xs:pl-2 lg:pl-8text-4xl mt-4 mb-8">
-            {render(blok.Title)}
-          </div>
-          <div className="flex justify-center items-center text-center">
-            <div className="p-6 mx-4 bg-three-box-panel flex flex-col">
-              <div className="lg:mb-0 text-grey font-bold text-3xl mb-4">
+        <div
+          className="flex flex-col mx-6"
+          style={{ maxWidth: "1024px", margin: "0 auto" }}
+        >
+          <div className="text-4xl my-8">{render(blok.Title)}</div>
+          <div className="grid grid-cols-3 gap-6 mt-12">
+            <div className="col-span-1 py-6 px-12 bg-three-box-panel flex flex-col">
+              <div className=" text-grey font-bold text-4xl my-6">
                 {render(blok.BoxTitle1)}
                 <span className="text-melon">.</span>
               </div>
-              <div className="lg:mb-0 text-md">{render(blok.BoxCaption1)}</div>
+              <div className="text-md text-black font-bold">
+                {render(blok.BoxCaption1)}
+              </div>
             </div>
-            <div className="p-6 mx-4 bg-three-box-panel flex flex-col">
-              <div className="lg:mb-0 text-grey font-bold text-3xl mb-4">
+            <div className="col-span-1 py-6 px-12 bg-three-box-panel flex flex-col">
+              <div className="text-grey font-bold text-4xl my-6">
                 {render(blok.BoxTitle2)}
                 <span className="text-melon">.</span>
               </div>
-              <div className="lg:mb-0 text-md">{render(blok.BoxCaption2)}</div>
+              <div className="text-md text-black font-bold">
+                {render(blok.BoxCaption2)}
+              </div>
             </div>
-            <div className="p-6 mx-4 bg-three-box-panel flex flex-col">
-              <div className="lg:mb-0 text-grey font-bold text-3xl mb-4">
+            <div className="col-span-1 py-6 px-8 bg-three-box-panel flex flex-col">
+              <div className="text-grey font-bold text-4xl my-6">
                 {render(blok.BoxTitle3)}
                 <span className="text-melon">.</span>
               </div>
-              <div className="lg:mb-0 text-md">{render(blok.BoxCaption3)}</div>
+              <div className="text-md text-black font-bold">
+                {render(blok.BoxCaption3)}
+              </div>
             </div>
           </div>
         </div>
