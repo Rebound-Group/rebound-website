98d3fe440463acdbfd51a10d895b24fc036d15b0
Nck Lowman <nlowman20@gmail.com>
2023-11-27 17:06:48 +1100

    Generational impact panel

--- components/StatsCarousel.js
+++ components/StatsCarousel.js
@@ -6,7 +6,7 @@ import { useSpringCarousel } from "react-spring-carousel";
 const StatsCarousel = ({ blok }) => {
   const Slide = ({ slide }) => {
     return (
-      <div className="wrapper mx-4 md:mx-12 w-full flex flex-col justify-center items-center ">
+      <div className="wrapper mx-4 md:mx-12 w-full flex flex-col justify-center items-center">
         <div
           className="flex text-2xl font-bold text-white p-4 md:p-9 mb-6 justify-center items-center grow-1 w-full border border-white rounded-xl"
           style={{
@@ -54,7 +54,9 @@ const StatsCarousel = ({ blok }) => {
   return (
     <div
       className={styles.StatsCarousel}
-      style={{ backgroundImage: `url(${blok.background_image.filename})` }}
+      style={{
+        backgroundImage: `url(${blok.background_image.filename})`,
+      }}
     >
       <div className="xs:py-8 xs:px-4 md:px-8 md:py-[110px] max-w-[100%] flex justify-center items-center">
         {blok.stats_list.length > 1 && (
--- components/home/ThreeBoxPanel.js
+++ components/home/ThreeBoxPanel.js
@@ -4,7 +4,7 @@ const ThreeBoxPanel = ({ blok }) => {
   return (
     <section>
       <div
-        className="flex justify-center items-start text-center pt-4"
+        className="flex justify-center items-start text-center py-4"
         style={{
           minHeight: "500px",
           backgroundImage: `url(${blok.Background.filename})`,
@@ -12,13 +12,13 @@ const ThreeBoxPanel = ({ blok }) => {
         }}
       >
         <div
-          className="flex flex-col mx-6"
+          className="flex flex-col mx-2"
           style={{ maxWidth: "1024px", margin: "0 auto" }}
         >
-          <div className="text-4xl my-8">{render(blok.Title)}</div>
-          <div className="grid grid-cols-3 gap-6 mt-12">
+          <div className="my-4 md:mb-12 md:mt-6 mx-6">{render(blok.Title)}</div>
+          <div className="grid mx-12 md:mx-6 grid-cols-1 md:grid-cols-3 gap-6 mt-6 md:mt-12 mb-8 md:mb-2">
             <div className="col-span-1 py-6 px-12 bg-three-box-panel flex flex-col">
-              <div className=" text-grey font-bold text-4xl my-6">
+              <div className="text-grey font-bold text-4xl my-4 md:my-6">
                 {render(blok.BoxTitle1)}
                 <span className="text-melon">.</span>
               </div>
@@ -27,7 +27,7 @@ const ThreeBoxPanel = ({ blok }) => {
               </div>
             </div>
             <div className="col-span-1 py-6 px-12 bg-three-box-panel flex flex-col">
-              <div className="text-grey font-bold text-4xl my-6">
+              <div className="text-grey font-bold text-4xl my-4 md:my-6">
                 {render(blok.BoxTitle2)}
                 <span className="text-melon">.</span>
               </div>
@@ -35,8 +35,8 @@ const ThreeBoxPanel = ({ blok }) => {
                 {render(blok.BoxCaption2)}
               </div>
             </div>
-            <div className="col-span-1 py-6 px-8 bg-three-box-panel flex flex-col">
-              <div className="text-grey font-bold text-4xl my-6">
+            <div className="col-span-1 py-6 px-6 bg-three-box-panel flex flex-col">
+              <div className="text-grey font-bold text-4xl my-4 md:my-6">
                 {render(blok.BoxTitle3)}
                 <span className="text-melon">.</span>
               </div>
