872cf7e182391f375c2b183034e374350a7601f3
Nck Lowman <nlowman20@gmail.com>
2023-11-29 12:57:06 +1100

    Spacing 3 boxes and image height

--- components/home/ThreeBoxPanel.js
+++ components/home/ThreeBoxPanel.js
@@ -6,18 +6,18 @@ const ThreeBoxPanel = ({ blok }) => {
       <div
         className="flex justify-center items-start text-center py-4"
         style={{
-          minHeight: "500px",
+          minHeight: "560px",
           backgroundImage: `url(${blok.Background.filename})`,
           backgroundSize: "cover",
         }}
       >
         <div
           className="flex flex-col mx-2"
-          style={{ maxWidth: "1024px", margin: "0 auto" }}
+          style={{ maxWidth: "1400px", margin: "0 auto" }}
         >
           <div className="my-4 md:mb-8 md:mt-6 mx-6">{render(blok.Title)}</div>
-          <div className="grid mx-12 md:mx-6 grid-cols-1 md:grid-cols-3 gap-6 mt-6 md:mt-12 mb-8 md:mb-2">
-            <div className="col-span-1 py-6 px-10 bg-three-box-panel flex flex-col">
+          <div className="grid mx-12 md:mx-6 grid-cols-1 md:grid-cols-3 gap-12 mt-6 md:mt-12 mb-8 md:mb-2">
+            <div className="col-span-1 py-6 xs:px-4 md:px-8 lg:px-12 bg-three-box-panel flex flex-col">
               <div className="text-grey font-bold text-4xl my-6 md:my-6">
                 {render(blok.BoxTitle1)}
                 <span className="text-melon">.</span>
@@ -35,7 +35,7 @@ const ThreeBoxPanel = ({ blok }) => {
                 {render(blok.BoxCaption2)}
               </div>
             </div>
-            <div className="col-span-1 py-6 px-6 bg-three-box-panel flex flex-col">
+            <div className="col-span-1 py-6 xs:px-4 md:px-8 lg:px-12 bg-three-box-panel flex flex-col">
               <div className="text-grey font-bold text-4xl my-6 md:my-6">
                 {render(blok.BoxTitle3)}
                 <span className="text-melon">.</span>
